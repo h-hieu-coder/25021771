88cad8ba0c4662efa7e83901960c122f0c632db3
mattroinho93 <huyhieula3@gmail.com>
2025-11-04 15:52:33 +0630

    chinh sua hi

--- hi.py
+++ hi.py
@@ -1,3 +1,22 @@
-print("hi")
-a = 0
-b = 0
+from math import sqrt
+import sys 
+sys.stdin = open("input.txt", "r")
+sys.stdout = open("output.txt", "w")
+def kt_nto(n) : 
+    if n<=0 : 
+        return False 
+    dem = 0
+    can = int(sqrt(n))
+    for i in range(1,can+1):
+        if n%i == 0 :
+            dem += 1
+    if dem == 1 : 
+        return True 
+    else :
+        return False 
+n = int(input())
+if kt_nto(n) :
+    print("n la so nguyen to")
+else :
+    print("n khong la so nguyen to")
+
